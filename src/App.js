96db1644ebc3b4e392b9ed36a6f28009bedd0250
gamgine <victor.fromentin95@ynov.com>
2018-05-14 11:36:37 +0000

    add composant

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MonComposant from './MonComposant.js';
 
 class App extends Component {
   render() {
@@ -12,7 +13,8 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+			<MonComposant/>
+		</p>
       </div>
     );
   }
---
+++ src/MonComposant.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react';
+
+export default class MonComposant extends Component {
+  render() {
+    return(<div> nouveau composant </div>);
+  }
+}
